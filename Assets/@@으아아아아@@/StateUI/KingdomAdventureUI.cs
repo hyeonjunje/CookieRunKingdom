553eb6fe80c25c1fa485ed08f26d5fe71921a67b
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2023-06-27 16:29:25 +0900

    월드 1 추가

--- Assets/@@으아아아아@@/StateUI/KingdomAdventureUI.cs
+++ Assets/@@으아아아아@@/StateUI/KingdomAdventureUI.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class KingdomAdventureUI : BaseUI
 {
-    
+    [SerializeField] private GameObject _cookigKingdom;
+    [SerializeField] private GameObject _world1;
+
+    public override void Show()
+    {
+        base.Show();
+
+        _cookigKingdom.SetActive(false);
+        _world1.SetActive(true);
+        // 월드 보이게
+    }
+
+    public override void Hide()
+    {
+        // 월드 안 보이게
+        _cookigKingdom.SetActive(true);
+        _world1.SetActive(false);
+
+        base.Hide();
+    }
 }
