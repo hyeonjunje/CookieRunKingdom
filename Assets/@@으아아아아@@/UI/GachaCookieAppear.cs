b4c90ae9e8bfc44c70e66a74630556246f73644e
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2023-07-12 10:44:57 +0900

    [feat] 가챠 입력하면 다음으로 가게 변경

--- Assets/@@으아아아아@@/UI/GachaCookieAppear.cs
+++ Assets/@@으아아아아@@/UI/GachaCookieAppear.cs
@@ -12,7 +12,8 @@ public class GachaCookieAppear : BaseUI
 {
     [SerializeField] private GameObject _cookieAppearUI;
     [SerializeField] private SkeletonGraphic _skeletonGrapic;
-    [SerializeField] private TextMeshProUGUI _cookiebubbleSpeech;
+    [SerializeField] private Transform _cookieBubbleSpeech;
+    [SerializeField] private TextMeshProUGUI _cookiebubbleSpeechText;
     [SerializeField] private TextMeshProUGUI _cookieSpeechText;
 
     [SerializeField] private Image _cookieGradeImage;
@@ -24,6 +25,51 @@ public class GachaCookieAppear : BaseUI
     private Camera _camera;
     private Vector3 _originPos;
     private float _orthographicSize;
+
+    private Sequence _speechSeq;
+    private Sequence _appearSeq;
+
+    private int _clickCount;
+
+    private bool _isSkip = false;
+
+    public int ClickCount
+    {
+        get { return _clickCount; }
+        set
+        {
+            _clickCount = value;
+
+            if (_clickCount == 0)
+            {
+                _cookieSpeechText.transform.localScale = Vector3.zero;
+                _cookieSpeechText.text = _cookieData.AppearSpeech;
+
+                _appearSeq.Pause();
+                _speechSeq.Restart();
+            }
+            else if(_clickCount == 1)
+            {
+                _cookieAppearUI.SetActive(true);
+                _skeletonGrapic.skeletonDataAsset = _cookieData.CookiePortrait;
+                _skeletonGrapic.Initialize(true);
+                _skeletonGrapic.AnimationState.SetAnimation(0, "idle", true);
+                _cookiebubbleSpeechText.text = _cookieData.BubbleSpeech;
+                _cookieGradeImage.sprite = _cookieData.CookieGradeSprite;
+                _cookieNameText.text = _cookieData.CharacterName;
+
+                _cookieBubbleSpeech.transform.localScale = Vector3.zero;
+                _speechSeq.Pause();
+                _appearSeq.Restart();
+            }
+            else if(_clickCount == 2)
+            {
+                _appearSeq.Pause();
+                _cookieAppearUI.SetActive(false);
+                GameManager.UI.PopUI();
+            }
+        }
+    }
 
     public void InitCookie(CookieData cookieData)
     {
@@ -45,11 +91,32 @@ public class GachaCookieAppear : BaseUI
 
         _camera = Camera.main;
         _manager = FindObjectOfType<KingdomManager>();
+
+        _speechSeq = DOTween.Sequence();
+        _appearSeq = DOTween.Sequence();
+
+        // speechSeq는 대사 나오고 별 뿌리고 약 4초
+        // appearSeq는 캐릭터 나오고 말풍선나오고 => 마우스 입력이 있어야만 끝낼 수 있음
+
+        _speechSeq.SetAutoKill(false).Pause()
+            .AppendInterval(0.05f)
+            .AppendCallback(() => _isSkip = true)
+            .Append(_cookieSpeechText.transform.DOScale(Vector3.one, 0.8f))
+            .AppendInterval(2f)
+            .Append(_cookieSpeechText.transform.DOScale(Vector3.zero, 0.5f))
+            .OnComplete(() => ClickCount = 1);
+
+        _appearSeq.SetAutoKill(false).Pause()
+            .Append(_cookieBubbleSpeech.DOScale(new Vector3(-1, 1, 1), 0.3f))
+            .SetEase(Ease.InBounce);
     }
 
     public override void Show()
     {
         base.Show();
+
+        ClickCount = 0;
+        _isSkip = false;
 
         _manager.IsMoveCamera = false;
         _originPos = _camera.transform.position;
@@ -57,29 +124,13 @@ public class GachaCookieAppear : BaseUI
 
         _camera.transform.position = Vector3.forward * -10;
         _camera.orthographicSize = 11;
+    }
 
-        _cookieSpeechText.transform.localScale = Vector3.zero;
-        _cookieSpeechText.text = _cookieData.AppearSpeech;
-
-        Sequence seq = DOTween.Sequence();
-        seq.Append(_cookieSpeechText.transform.DOScale(Vector3.one, 0.3f))
-            .SetEase(Ease.OutBack)
-            .AppendInterval(3f)
-            .AppendCallback(() =>
-            {
-                _cookieAppearUI.SetActive(true);
-                _skeletonGrapic.skeletonDataAsset = _cookieData.CookiePortrait;
-                _skeletonGrapic.Initialize(true);
-                _skeletonGrapic.AnimationState.SetAnimation(0, "idle", true);
-                _cookiebubbleSpeech.text = _cookieData.BubbleSpeech;
-                _cookieGradeImage.sprite = _cookieData.CookieGradeSprite;
-                _cookieNameText.text = _cookieData.CharacterName;
-            })
-            .AppendInterval(3f)
-            .OnComplete(() =>
-            {
-                _cookieAppearUI.SetActive(false);
-                GameManager.UI.PopUI();
-            });
+    public void OnClick(InputAction.CallbackContext value)
+    {
+        if(value.started && _isSkip)
+        {
+            ClickCount++;
+        }
     }
 }
